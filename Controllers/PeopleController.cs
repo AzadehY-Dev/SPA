ec2ce5bb7a97577968ac3c1ca707dc2ee683eac2
CarlaY <47314411+CarlaY-Dev@users.noreply.github.com>
2023-11-20 14:32:11 +0330

    Single Page Application with mvc and web API

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using SPA.Data;
+using SPA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SPA.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        SPAContext db=new SPAContext();
+        //get: get info from db
+        public IEnumerable<Person> GetPeople()
+        {
+            return db.People;
+        }
+        public Person GetByID(int id)
+        {
+            return db.People.Find(id);
+        }
+        //post: insert into db
+        public void PostPerson(Person person)
+        {
+            db.People.Add(person);
+            db.SaveChanges();
+        }
+        //put: edit and update data
+        public void PutPerson(int id, Person person)
+        {
+            if(id==person.ID)
+            {
+                db.Entry(person).State=System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+        public void DeletePerson(int id) 
+        {
+            var person = GetByID(id);
+            db.Entry(person).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace SPA.Models
+{
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Please enter {0}")]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        [Display(Name = "Family")]
+        [Required(ErrorMessage = "Please enter {0}")]
+        [MaxLength(150)]
+        public string Family { get; set; }
+        [Required(ErrorMessage = "Please enter {0}")]
+        public int Age { get; set; }
+    }
+}
